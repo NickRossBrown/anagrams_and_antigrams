fb130ffd7ffb81d6e07a0442818908c0a8d03223
Guest User <Guest@Epicodus-3C.local>
2018-06-29 10:16:08 -0700

    test 1 pass

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -2,4 +2,7 @@ class Anagram_antigram
   def initialize (words_input)
     @inputted_words = words_input
   end
+  def anagrams()
+    @inputted_words
+  end
 end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -4,7 +4,7 @@ require 'anagram_antigram'
 
 describe ('Anagrams_and_antigrams') do
   it ("returns a string") do
-    anagrams_and_antigrams = new Anagram_antigram
-    expect(anagrams_and_antigrams("string")).to(eq("string"))
+    anagrams_and_antigrams = Anagram_antigram.new("string")
+    expect(anagrams_and_antigrams.anagrams()).to(eq("string"))
   end
 end
