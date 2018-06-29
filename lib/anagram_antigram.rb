abaa27ef1b75b8a08e9a8ee46524a7fe7f3e0dd2
Guest User <Guest@Epicodus-3C.local>
2018-06-29 12:39:32 -0700

    take string and change all the letters to lower case

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -1,8 +1,26 @@
 class Anagram_antigram
   def initialize (words_input)
     @inputted_words = words_input
+
   end
   def anagrams()
-    @inputted_words
+    plitted_wordseach do |letter|
+      splitted_words = splitted_words.unshift(letter)
+    end
+    splitted_words = @inputted_words.split(" ")
+    array = []
+    array2 = []
+    splitted_words.each do |word|
+      if array.include?(word)
+        array2.push(word)
+        puts "REACHED"
+      end
+      array.push(word)
+      puts word
+    end
+    splitted_wordseach do |letter|
+      splitted_words = splitted_words.unshift(letter)
+    end
+    array2
   end
 end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -8,7 +8,7 @@ describe ('Anagrams_and_antigrams') do
     expect(anagrams_and_antigrams.anagrams()).to(eq("string"))
   end
   it ("returns a true statement if two words are a anagrams") do
-    anagrams_and_antigrams = Anagram_antigram.new("tea eat")
-    expect(anagrams_and_antigrams.anagrams()).to(eq(true)) 
+    anagrams_and_antigrams = Anagram_antigram.new("tea eat tea")
+    expect(anagrams_and_antigrams.anagrams()).to(eq(true))
   end
 end
