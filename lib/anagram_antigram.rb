aeb7e5cf8508b5467f0851856267a1beecbd3797
Guest User <Guest@Epicodus-3C.local>
2018-06-29 10:02:00 -0700

    initialized variable(s)

---
+++ lib/anagram_antigram.rb
@@ -0,0 +1,5 @@
+class Anagram_antigram
+  def initialize (words_input)
+    @inputted_words = words_input
+  end
+end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -4,6 +4,7 @@ require 'anagram_antigram'
 
 describe ('Anagrams_and_antigrams') do
   it ("returns a string") do
+    anagrams_and_antigrams = new Anagram_antigram
     expect(anagrams_and_antigrams("string")).to(eq("string"))
   end
 end
