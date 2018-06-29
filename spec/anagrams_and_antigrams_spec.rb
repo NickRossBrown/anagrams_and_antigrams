24c8eb94b3cb267f4a936be73f92ce87cee2df6a
Guest User <Guest@Epicodus-3C.local>
2018-06-29 10:24:10 -0700

    test 2 false positive

--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -7,4 +7,8 @@ describe ('Anagrams_and_antigrams') do
     anagrams_and_antigrams = Anagram_antigram.new("string")
     expect(anagrams_and_antigrams.anagrams()).to(eq("string"))
   end
+  it ("returns a true statement if two words are a anagrams") do
+    anagrams_and_antigrams = Anagram_antigram.new("tea eat")
+    expect(anagrams_and_antigrams.anagrams()).to(eq(true))
+  end
 end
